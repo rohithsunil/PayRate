479db07d3fb0c76247b7d7013f7e7a57f0778f71
Aadithye N <aadithyen@gmail.com>
2022-07-20 18:25:46 +0530

    Implemented BitRate conversion.

--- js/script.js
+++ js/script.js
@@ -16,38 +16,47 @@ function changeCurr() {
 		document.querySelector(".bitHeader").classList.add("hidden");
 		document.querySelector(".payHeader").classList.remove("hidden");
 	}
-  populateList();
-  getExchangeRate()
+	populateList();
+	getExchangeRate();
 }
 
 function populateList() {
 	for (let i = 0; i < dropList.length; i++) {
-		if (!toggleButton.checked) {
-			for (let currency_code in country_list) {
-				// selecting AED by default as FROM currency and INR as TO currency
-				let selected =
-					i == 0
-						? currency_code == "AED"
-							? "selected"
-							: ""
-						: currency_code == "INR"
+		dropList[i].innerHTML = "";
+		for (let currency_code in country_list) {
+			// selecting AED by default as FROM currency and INR as TO currency
+			let selected =
+				i == 0
+					? currency_code == "AED"
 						? "selected"
-						: "";
-				// creating option tag with passing currency code as a text and value
-				let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
-				// inserting options tag inside select tag
-				dropList[i].insertAdjacentHTML("beforeend", optionTag);
-			}
-		} else {
-			dropList[1].innerHTML = "";
-			for (let crypto_code in crypto_list) {
-				let currency_code = "BTC" ? "selected" : "";
-				let optionTag = `<option value="${crypto_code}" ${selected}>${crypto_code}</option>`;
-				// inserting options tag inside select tag
-				dropList[1].insertAdjacentHTML("beforeend", optionTag);
-			}
+						: ""
+					: currency_code == "INR"
+					? "selected"
+					: "";
+			// creating option tag with passing currency code as a text and value
+			let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+			// inserting options tag inside select tag
+			dropList[i].insertAdjacentHTML("beforeend", optionTag);
 		}
-		dropList[i].addEventListener("change", (e) => {
+		dropList[1].addEventListener("change", (e) => {
+			loadFlag(e.target); // calling loadFlag with passing target element as an argument
+		});
+	}
+
+	if (toggleButton.checked) {
+		dropList[0].innerHTML = "";
+		for (let crypto_code in crypto_list) {
+			let selected = crypto_code == "BTC" ? "selected" : "";
+			let optionTag = `<option value="${crypto_code}" ${selected}>${crypto_code}</option>`;
+			// inserting options tag inside select tag
+			dropList[0].insertAdjacentHTML("beforeend", optionTag);
+		}
+		dropList[0].parentElement.querySelector("img").src =
+			"https://upload.wikimedia.org/wikipedia/commons/1/1d/No_image.svg";
+	} else {
+		loadFlag(fromCurrency); // calling loadFlag with passing select element (fromCurrency) of FROM
+		// calling loadFlag with passing target element as an argument
+		dropList[0].addEventListener("change", (e) => {
 			loadFlag(e.target); // calling loadFlag with passing target element as an argument
 		});
 	}
@@ -95,17 +104,46 @@ function getExchangeRate() {
 		amountVal = 1;
 	}
 	exchangeRateTxt.innerText = "Getting exchange rate...";
-	let url = `https://v6.exchangerate-api.com/v6/07918a05543d669af668dab4/latest/${fromCurrency.value}`;
-	// fetching api response and returning it with parsing into js obj and in another then method receiving that obj
-	fetch(url)
-		.then((response) => response.json())
-		.then((result) => {
-			let exchangeRate = result.conversion_rates[toCurrency.value]; // getting user selected TO currency rate
-			let totalExRate = (amountVal * exchangeRate).toFixed(2); // multiplying user entered value with selected TO currency rate
-			exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
-		})
-		.catch(() => {
-			// if user is offline or any other error occured while fetching data then catch function will run
-			exchangeRateTxt.innerText = "Something went wrong";
-		});
+
+	if (toggleButton.checked) {
+		let toCryptoUrl = `https://min-api.cryptocompare.com/data/price?fsym=${fromCurrency.value}&tsyms=USD`;
+		let toCurrUrl = `https://v6.exchangerate-api.com/v6/07918a05543d669af668dab4/pair/USD/${toCurrency.value}`;
+		// fetching api response and returning it with parsing into js obj and in another then method receiving that obj
+
+		fetch(toCryptoUrl)
+			.then((response) => response.json())
+			.then((result) => {
+				let coinToUSDRate = result["USD"];
+				let usdToCurRate; // getting user selected TO currency rate // multiplying user entered value with selected TO currency rate
+				fetch(toCurrUrl)
+					.then((response) => response.json())
+					.then((result) => {
+						usdToCurRate = result.conversion_rate;
+						let totalExRate = (
+							amountVal *
+							coinToUSDRate *
+							usdToCurRate
+						).toFixed(2);
+						exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
+					});
+			})
+			.catch(() => {
+				// if user is offline or any other error occured while fetching data then catch function will run
+				exchangeRateTxt.innerText = "Something went wrong";
+			});
+	} else {
+		let url = `https://v6.exchangerate-api.com/v6/07918a05543d669af668dab4/latest/${fromCurrency.value}`;
+		// fetching api response and returning it with parsing into js obj and in another then method receiving that obj
+		fetch(url)
+			.then((response) => response.json())
+			.then((result) => {
+				let exchangeRate = result.conversion_rates[toCurrency.value]; // getting user selected TO currency rate
+				let totalExRate = (amountVal * exchangeRate).toFixed(2); // multiplying user entered value with selected TO currency rate
+				exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
+			})
+			.catch(() => {
+				// if user is offline or any other error occured while fetching data then catch function will run
+				exchangeRateTxt.innerText = "Something went wrong";
+			});
+	}
 }
